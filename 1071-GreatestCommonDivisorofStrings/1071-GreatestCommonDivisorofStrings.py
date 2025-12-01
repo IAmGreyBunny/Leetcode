fd68a9a1a7e5db188fb283e7a9eb55215e43096e
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-01 16:04:14 +0800

    File updated at 12/1/2025, 4:04:13 PM

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -1,21 +1,20 @@
-# Last updated: 12/1/2025, 4:03:51 PM
+# Last updated: 12/1/2025, 4:04:13 PM
 1class Solution:
 2    def gcdOfStrings(self, str1: str, str2: str) -> str:
-3        import math
-4
-5        divisor = ""
-6        
-7        if str1+str2 == str2+str1:
-8            pass
-9        else:
-10            return ""
-11        
-12        i=0
-13        while(i<len(str1) and i<len(str2)):
-14            if str1[i] == str2[i]:
-15                divisor += str1[i]
-16            else:
-17                break
-18            i+=1
-19
-20        return divisor[:math.gcd(len(str1),len(str2))]
+3
+4        divisor = ""
+5        
+6        if str1+str2 == str2+str1:
+7            pass
+8        else:
+9            return ""
+10        
+11        i=0
+12        while(i<len(str1) and i<len(str2)):
+13            if str1[i] == str2[i]:
+14                divisor += str1[i]
+15            else:
+16                break
+17            i+=1
+18
+19        return divisor[:math.gcd(len(str1),len(str2))]
