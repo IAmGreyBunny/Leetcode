87d0c23d677a4ff0525b841c43b56f543fd9f6ab
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-04 14:49:07 +0800

    File updated at 12/4/2025, 2:49:06 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,4 +1,4 @@
-# Last updated: 12/4/2025, 2:39:58 PM
+# Last updated: 12/4/2025, 2:49:06 PM
 1class Solution:
 2    def reverseWords(self, s: str) -> str:
 3        
@@ -17,11 +17,11 @@
 16                i+=1
 17            elif i==len(s):
 18                if len(s[j:i]):
-19                    words.insert(0,s[j:i])
+19                    words.append(s[j:i])
 20                break
 21            else:
 22                if len(s[j:i])>0:
-23                    words.insert(0,s[j:i])
+23                    words.append(s[j:i])
 24                
 25                # set j to the end of the word
 26                j = i
@@ -29,7 +29,7 @@
 28                while j<len(s) and s[j] is " ":
 29                    j+=1
 30                i = j
-31        
+31        words.reverse()
 32        return " ".join(words)
 33
 34
