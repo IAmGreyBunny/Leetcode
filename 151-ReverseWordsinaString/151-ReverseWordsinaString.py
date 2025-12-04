e6967a6c51342d366a746b8794f9f9b16aef3930
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-04 14:39:59 +0800

    File updated at 12/4/2025, 2:39:58 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,25 +1,36 @@
-# Last updated: 12/4/2025, 2:06:08 PM
+# Last updated: 12/4/2025, 2:39:58 PM
 1class Solution:
 2    def reverseWords(self, s: str) -> str:
-3        list_string = list(s)
-4        word_stack = []
+3        
+4        words = []
 5
-6        word = ""
-7        for char in list_string:
-8            if char.isalnum():
-9                word+=char
-10            else:
-11                if word != "":
-12                    word_stack.append(word)
-13                word = ""
-14            
-15        if word != "":
-16            word_stack.append(word)
-17
-18        result = ""
-19        while len(word_stack):
-20            word = word_stack.pop()
-21            if result != "":
-22                result += " " 
-23            result += word
-24        return result
+6        # Edge cases
+7        if len(s) <=1:
+8            return s
+9        
+10        # Extract words
+11        i=0
+12        j=0
+13
+14        while i<=len(s):
+15            if i<len(s) and s[i] is not " ":
+16                i+=1
+17            elif i==len(s):
+18                if len(s[j:i]):
+19                    words.insert(0,s[j:i])
+20                break
+21            else:
+22                if len(s[j:i])>0:
+23                    words.insert(0,s[j:i])
+24                
+25                # set j to the end of the word
+26                j = i
+27                # skip spaces
+28                while j<len(s) and s[j] is " ":
+29                    j+=1
+30                i = j
+31        
+32        return " ".join(words)
+33
+34
+35            
