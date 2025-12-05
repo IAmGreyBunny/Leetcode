339154df48d9185f75c14d06be1e17c830270bda
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-05 17:32:59 +0800

    File updated at 12/5/2025, 5:32:57 PM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,20 +1,32 @@
-# Last updated: 12/5/2025, 5:32:31 PM
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        length = len(nums)
-        back = length - 1
-        while back >= 0 and nums[back] == 0:
-            back -= 1
-        back += 1
-
-        front = 0
-        while front < back:
-            if nums[front] == 0:
-                nums[front:back - 1] = nums[front + 1: back]
-                nums[back - 1] = 0
-                back -= 1
-            else:
-                front += 1
+# Last updated: 12/5/2025, 5:32:57 PM
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        i = 0 
+7        j = 0 #points to the first 0 from the left
+8
+9        if len(nums)<=1:
+10            return nums
+11        
+12
+13        while i<len(nums) and j<len(nums):
+14            if nums[i]==0:
+15                if i<j:
+16                    j=i
+17                i+=1
+18            else:
+19                nums[i],nums[j] = nums[j],nums[i]
+20                while j<=i:
+21                    if nums[j]==0:
+22                        break
+23                    else:
+24                        j+=1
+25                i+=1
+26
+27        
+28
+29        
+30
+31
