ecd3d3227afa48fcb41da5f9a41478eec7139456
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-07 15:57:44 +0800

    File updated at 12/7/2025, 3:57:43 PM

--- 392-IsSubsequence/392-IsSubsequence.py
+++ 392-IsSubsequence/392-IsSubsequence.py
@@ -1,21 +1,21 @@
-# Last updated: 11/30/2025, 5:39:51 PM
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-
-        start_search = 0
-
-        i = 0
-
-        for j in range(0,len(t)):
-            
-            if i == len(s):
-                break
-
-            if t[j] == s[i]:
-                print(f"Comparing {s[i]} with {t[j]}")
-                i+=1
-        
-        if i == len(s):
-            return True
-        else:
-            return False
+# Last updated: 12/7/2025, 3:57:43 PM
+1class Solution:
+2    def isSubsequence(self, s: str, t: str) -> bool:
+3
+4        start_search = 0
+5
+6        i = 0
+7
+8        for j in range(0,len(t)):
+9            
+10            if i == len(s):
+11                break
+12
+13            if t[j] == s[i]:
+14                print(f"Comparing {s[i]} with {t[j]}")
+15                i+=1
+16        
+17        if i == len(s):
+18            return True
+19        else:
+20            return False
