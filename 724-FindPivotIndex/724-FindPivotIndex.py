d60644592e9b2394e7ff6e468dbb325f3c7591bb
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-08 20:54:48 +0800

    File updated at 12/8/2025, 8:54:47 PM

--- 724-FindPivotIndex/724-FindPivotIndex.py
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -1,14 +1,15 @@
-# Last updated: 12/8/2025, 8:54:28 PM
+# Last updated: 12/8/2025, 8:54:47 PM
 1class Solution:
 2    def pivotIndex(self, nums: List[int]) -> int:
 3        if len(nums)<1:
 4            return -1
-5        total_sum = sum(nums)
-6
-7        left_sum = 0
-8        for i in range(0,len(nums)):
-9            if left_sum==(total_sum - left_sum - nums[i]):
-10                return i
-11            left_sum+=nums[i]
-12        
-13        return -1
+5            
+6        total_sum = sum(nums)
+7
+8        left_sum = 0
+9        for i in range(0,len(nums)):
+10            if left_sum==(total_sum - left_sum - nums[i]):
+11                return i
+12            left_sum+=nums[i]
+13        
+14        return -1
