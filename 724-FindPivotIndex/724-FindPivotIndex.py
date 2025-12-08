81c3fa8c619443a08825aa6ddb5a046c589babae
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-08 20:52:31 +0800

    File updated at 12/8/2025, 8:52:31 PM

--- 724-FindPivotIndex/724-FindPivotIndex.py
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -1,26 +1,14 @@
-# Last updated: 12/8/2025, 6:47:02 PM
+# Last updated: 12/8/2025, 8:52:31 PM
 1class Solution:
 2    def pivotIndex(self, nums: List[int]) -> int:
-3        if len(nums) < 1:
-4            return -1
+3        total_sum = sum(nums)
+4        print(total_sum)
 5
-6        left_sum = [nums[0]]
-7        right_sum = [nums[len(nums)-1]]
-8
-9        for i in range(1,len(nums)):
-10            left_sum.append(left_sum[-1] + nums[i])
-11            right_sum.append(right_sum[-1] + nums[len(nums)-1-i])
+6        left_sum = 0
+7        for i in range(0,len(nums)):
+8            right_sum = total_sum - left_sum - nums[i]
+9            if left_sum==right_sum:
+10                return i
+11            left_sum+=nums[i]
 12        
-13        right_sum.reverse()
-14
-15        print(f"L:{left_sum}\nR:{right_sum}")
-16
-17        for i in range(0,len(nums)):
-18            if i==0 and left_sum[0]==right_sum[0]:
-19                return 0 
-20            elif i == len(nums)-1 and left_sum[len(nums)-1] == right_sum[len(nums)-1]:
-21                return len(nums)-1
-22            elif i+2 < len(nums) and left_sum[i]==right_sum[i+2]:
-23                return i+1
-24        
-25        return -1
+13        return -1
