b09b288ab8f9947f3924228f799a7ae41fb7a22f
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-11 18:29:12 +0800

    File updated at 12/11/2025, 6:29:12 PM

--- 394-DecodeString/394-DecodeString.py
+++ 394-DecodeString/394-DecodeString.py
@@ -1,4 +1,4 @@
-# Last updated: 12/11/2025, 6:28:33 PM
+# Last updated: 12/11/2025, 6:29:12 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -6,29 +6,27 @@
 5#         self.next = next
 6class Solution:
 7    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-8        if head == None:
-9            return None
-10        elif head.next == None:
-11            return head
-12
-13        prev = None
-14        current = head
-15
-16        while(current != None):
-17            if current.next != None:
-18                temp = current.next
-19            else:
-20                temp = None
-21            
-22            current.next = prev
-23            prev = current
-24            if temp != None:
-25                current = temp
-26            else:
-27                return current
+8        prev = None
+9        current = head
+10
+11        while(current != None):
+12            if current.next != None:
+13                temp = current.next
+14            else:
+15                temp = None
+16            
+17            current.next = prev
+18            prev = current
+19
+20            if temp != None:
+21                current = temp
+22            else:
+23                return current
+24        
+25        return current
+26
+27
 28
 29
-30
+30            
 31
-32            
-33
