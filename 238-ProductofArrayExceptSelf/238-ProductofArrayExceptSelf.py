1e39f8c4a049fc22952b19ba18794e2e6bba8e9b
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-12 22:24:05 +0800

    File updated at 12/12/2025, 10:24:04 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,17 +1,17 @@
-# Last updated: 12/12/2025, 10:22:37 PM
+# Last updated: 12/12/2025, 10:24:04 PM
 1class Solution:
 2    def productExceptSelf(self, nums: List[int]) -> List[int]:
 3        if len(nums) < 1:
 4            return []
 5
-6        prefix = [1]
-7        postfix = [1]
-8
-9        for i in range(1,len(nums)):
-10            prefix.append(prefix[i-1]*nums[i-1])
-11            postfix.append(postfix[i-1]*nums[len(nums)-i])
-12
-13        answers = []
+6        answers = []
+7        prefix = [1]
+8        postfix = [1]
+9
+10        for i in range(1,len(nums)):
+11            prefix.append(prefix[i-1]*nums[i-1])
+12            postfix.append(postfix[i-1]*nums[len(nums)-i])
+13        
 14        for i in range(0,len(prefix)):
 15            answers.append(prefix[i] * postfix[len(postfix)-1-i])
 16
