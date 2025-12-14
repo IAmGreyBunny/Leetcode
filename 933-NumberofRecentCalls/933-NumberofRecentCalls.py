9669f6902c3f574f35e7babf75f3af79273cf4ca
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-14 11:29:28 +0800

    File updated at 12/14/2025, 11:29:28 AM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.py
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.py
@@ -1,23 +1,18 @@
-# Last updated: 12/14/2025, 10:45:41 AM
+# Last updated: 12/14/2025, 11:29:28 AM
 1class RecentCounter:
 2
 3    def __init__(self):
-4        self.recent_reqs = []
-5        self.last_req_index = -1
-6        
-7    def ping(self, t: int) -> int:
-8        self.recent_reqs.append(t)
-9        recent_reqs=self.recent_reqs
-10        
-11        
-12        for req_index in range(0,len(recent_reqs)):
-13            if recent_reqs[req_index] >= t-3000:
-14                self.last_req_index = req_index
-15                break
-16    
-17        return len(recent_reqs)-self.last_req_index
-18
-19
-20# Your RecentCounter object will be instantiated and called as such:
-21# obj = RecentCounter()
-22# param_1 = obj.ping(t)
+4        self.queue = deque()
+5        
+6    def ping(self, t: int) -> int:
+7        self.queue.append(t)
+8
+9        # Remove outdated
+10        while self.queue and self.queue[0] < t-3000:
+11            self.queue.popleft()
+12
+13        return len(self.queue)
+14
+15# Your RecentCounter object will be instantiated and called as such:
+16# obj = RecentCounter()
+17# param_1 = obj.ping(t)
