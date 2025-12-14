4ea1dfb927ef8b668ab2c09d32d586282aff29f6
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-14 10:43:21 +0800

    File updated at 12/14/2025, 10:43:20 AM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.py
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.py
@@ -1,23 +1,24 @@
-# Last updated: 12/14/2025, 10:41:49 AM
+# Last updated: 12/14/2025, 10:43:20 AM
 1class RecentCounter:
 2
 3    def __init__(self):
 4        self.recent_reqs = []
-5        
-6    def ping(self, t: int) -> int:
-7        self.recent_reqs.append(t)
-8        recent_reqs=self.recent_reqs
-9        last_req_index = -1
+5        self.last_req_index = -1
+6        
+7    def ping(self, t: int) -> int:
+8        self.recent_reqs.append(t)
+9        recent_reqs=self.recent_reqs
 10        
-11        for req_index in range(0,len(recent_reqs)):
-12            if recent_reqs[req_index] >= t-3000:
-13                last_req_index = req_index
-14                break
-15    
-16        return len(recent_reqs)-last_req_index
-17        
-18
+11        
+12        for req_index in range(0,len(recent_reqs)):
+13            if recent_reqs[req_index] >= t-3000:
+14                self.last_req_index = req_index
+15                break
+16    
+17        return len(recent_reqs)-self.last_req_index
+18        
 19
-20# Your RecentCounter object will be instantiated and called as such:
-21# obj = RecentCounter()
-22# param_1 = obj.ping(t)
+20
+21# Your RecentCounter object will be instantiated and called as such:
+22# obj = RecentCounter()
+23# param_1 = obj.ping(t)
