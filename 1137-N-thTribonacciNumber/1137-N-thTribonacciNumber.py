ba17b4218cc9a108a66fc97d9e551867c7c8390b
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-17 10:21:26 +0800

    File updated at 12/17/2025, 10:21:25 AM

--- 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
+++ 1137-N-thTribonacciNumber/1137-N-thTribonacciNumber.py
@@ -1,10 +1,12 @@
-# Last updated: 12/17/2025, 10:17:12 AM
+# Last updated: 12/17/2025, 10:21:25 AM
 1class Solution: 
 2    def tribonacci(self, n: int) -> int:
 3        ans = [0,1,1]
-4        while n > len(ans)-1:
-5            ans.append(ans[len(ans)-3] + ans[len(ans)-2] + ans[len(ans)-1])
-6        
-7        return ans[n]
-8
-9        
+4        ans_len = len(ans)
+5        while n > ans_len-1:
+6            ans.append(ans[ans_len-3] + ans[ans_len-2] + ans[ans_len-1])
+7            ans_len+=1
+8        
+9        return ans[n]
+10
+11        
