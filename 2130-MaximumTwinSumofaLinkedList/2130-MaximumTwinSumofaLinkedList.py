c07745ffec87edc0177de545aefd01f507e9d3de
IAmGreyBunny <67668607+IAmGreyBunny@users.noreply.github.com>
2025-12-25 16:10:07 +0800

    File updated at 12/25/2025, 4:10:05 PM

--- 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
+++ 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 12/25/2025, 4:09:10 PM
+# Last updated: 12/25/2025, 4:10:05 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -22,15 +22,13 @@
 21            stack.append(cur.val)
 22            cur = cur.next
 23
-24        print(stack)
-25
-26        # second traversal
-27        cur = head
-28        max_val = 0
-29
-30        while(cur):
-31            cur_sum = cur.val + stack.pop()
-32            max_val = cur_sum if cur_sum > max_val else max_val
-33            cur = cur.next
-34
-35        return max_val
+24        # second traversal
+25        cur = head
+26        max_val = 0
+27
+28        while(cur):
+29            cur_sum = cur.val + stack.pop()
+30            max_val = cur_sum if cur_sum > max_val else max_val
+31            cur = cur.next
+32
+33        return max_val
